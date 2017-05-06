2f2f17996a6b03fde6bcaf86453f9aa077f86856
Bálint Villei <csernozjom1@gmail.com>
2017-05-06 17:58:04 +0200

    include Bootstrap

--- src/main/java/hu/bvillei/todolist/config/WebSecurityConfig.java
+++ src/main/java/hu/bvillei/todolist/config/WebSecurityConfig.java
@@ -20,6 +20,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
+            	.antMatchers("/css/**", "/js/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
